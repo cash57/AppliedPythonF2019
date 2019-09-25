d8b8b4a237c4160fefa01dd2ab20e9b6c8830102
cash57 <al.shafiev.sam@gmail.com>
2019-09-25 17:00:04 +0300

    fixed HW02

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -6,3 +6,23 @@ if __name__ == '__main__':
     filename = sys.argv[1]
 
     # Ваш код
+
+def main(file):
+    """
+    Функция по открытию говно файлов в читаемом виде
+    """
+    open_file(file)
+
+
+def open_file(file):
+        try:
+            somefile = open(file, "r")
+        except FileNotFoundError:
+            print("Файл не валиден")
+        except Exception:
+            print("Формат не валиден")
+
+def read_file():
+фыавыаыа
+фвфыаыа
+фыывфыфыа
